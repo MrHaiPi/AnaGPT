05845f1150dac510efe5e5f79d59ea54ce976335
‘xiarui’ <1600127471@qq.com>
2023-05-03 00:48:26 +0800

    xr

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import ast
 import os
 from ana_gpt import Anagpt
 
@@ -8,12 +9,12 @@ if __name__ == '__main__':
     if os.path.exists(file_name):
         f = open(file_name, 'r', encoding="utf-8")
         content = f.read()
+        content = ast.literal_eval(content)
         f.close()
-        start_index = content.find('version') + len('version') + 3
-        end_index = content.find('version') + len('version') + 6
-        version = content[start_index:end_index]
+        version = content['version']
     else:
         print('Warning: No version file is found!')
-    new_gpt = Anagpt(version)
+
+    new_gpt = Anagpt(str(version))
     new_gpt.chat()
 
